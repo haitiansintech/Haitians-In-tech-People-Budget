ff79203463907929949c90859b8247f030e6309c
Chanathip Gulgalkhai <kenobi1337@protonmail.com>
2022-08-04 14:04:18 -0400

    Completed back-end /budget route

--- app/controllers/Budget/budget.js
+++ app/controllers/Budget/budget.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
+const { parse } = require('csv');
+const fs = require("fs");
 
-router.get('/', (req, res) => {
-    res.send("budget API worked")
+
+
+router.get('/', async (req, res) => {
+    fs.createReadStream(__dirname+'../../../data/MEF-2021-2022-TEB-English.csv').pipe(parse({ columns: true }, (err, records) => {
+        let tempRecord = records;
+        for (let i = 0; i < tempRecord.length; i++) {
+            delete tempRecord[i]["112,895,190,744.66"];
+            delete tempRecord[i][""];
+            delete tempRecord[i]["$959,609,121.33 "];
+        }
+
+        if (req.query.category) {
+            tempRecord = tempRecord.filter(record => {
+                return record["Category"].toLowerCase() === req.query.category.toLowerCase();
+            })
+        }
+
+        if (req.query["governing body"]) {
+            tempRecord = tempRecord.filter(record => {
+                return record["Governing Body"].toLowerCase() === req.query["governing body"].toLowerCase();
+            })
+        }
+
+        if (req.query["governing function"]) {
+            tempRecord = tempRecord.filter(record => {
+                return record["Government Function"].toLowerCase() === req.query["governing function"].toLowerCase();
+            })
+        }
+
+        res.send(tempRecord)
+    }));
 })
 
 module.exports = { router };
